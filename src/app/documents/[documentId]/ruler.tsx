a50cd2950ea313f9301acd98ec6cc8865bb8871b
NodePulse <bharbeysachin@gmail.com>
2025-04-30 16:20:16 +0530

    Adding collaborative margin ruler features

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,4 +1,5 @@
 import { MarkerProps } from "@/types/type";
+import { useMutation, useStorage } from "@liveblocks/react";
 import React, { useRef, useState } from "react";
 import { FaCaretDown } from "react-icons/fa";
 
@@ -37,8 +38,15 @@ const Marker = ({
 };
 
 export const Ruler = () => {
-  const [leftMargin, setLeftMargin] = useState(56);
-  const [rightMargin, setRightMargin] = useState(56);
+  const leftMargin = useStorage((root) => root.leftMargin) ?? 56;
+  const setLeftMargin = useMutation(({ storage }, position: number) => {
+    storage.set("leftMargin", position);
+  }, []);
+
+  const rightMargin = useStorage((root) => root.rightMargin) ?? 56;
+  const setRightMargin = useMutation(({ storage }, position: number) => {
+    storage.set("rightMargin", position);
+  }, []);
 
   const [isDraggingLeft, setIsDraggingLeft] = useState(false);
   const [isDraggingRight, setIsDraggingRight] = useState(false);
